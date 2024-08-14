afe1cc8266cbb19a4456941a579a39d257424ec4
triflicacid <rubensaunders@hotmail.co.uk>
2024-08-14 10:54:33 +0100

    bug fixes to the TM1637 library

--- code/shared/tm1637.c
+++ code/shared/tm1637.c
@@ -1,6 +1,7 @@
 #include "tm1637.h"
 
 #include <stdio.h>
+#include <inttypes.h>
 
 #define TM1637_COMM1 0x40
 #define TM1637_COMM2 0xC0
@@ -9,7 +10,7 @@
 #define TM1637_DIGITS 4
 
 // map digits to segments
-const uint8_t digit_map[] = {
+const uint8_t digit_masks[] = {
   //XGFEDCBA
   0b00111111,    // 0
   0b00000110,    // 1
@@ -29,154 +30,102 @@ const uint8_t digit_map[] = {
   0b01110001     // F
 };
 
-// pin delay
 static void delay(void) {
-  HAL_Delay(1);
+	delay_us(1);
 }
 
-// start transfer -- lower data I/O pin
-static void tm1637_start(tm1637_t *tm) {
+static void start(tm1637_t *tm) {
   write_pin(tm->port, tm->dio, false);
   delay();
 }
 
-// end transfer -- raise data I/O pin
-static void tm1637_stop(tm1637_t *tm) {
-  write_pin(tm->port, tm->dio, false);
+static void stop(tm1637_t *tm) {
+	write_pin(tm->port, tm->dio, false);
+	delay();
+	write_pin(tm->port, tm->clk, true);
+	delay();
+	write_pin(tm->port, tm->dio, true);
+	delay();
+}
+
+static void read_ack(tm1637_t *tm) {
+  // don't actually read, emulate
+  write_pin(tm->port, tm->clk, false);
+  write_pin(tm->port, tm->dio, true);
   delay();
 
   write_pin(tm->port, tm->clk, true);
   delay();
 
-  write_pin(tm->port, tm->dio, true);
+  write_pin(tm->port, tm->dio, false);
+  delay();
+
+  write_pin(tm->port, tm->clk, false);
   delay();
 }
 
-// send a byte to the TM1637 module, return ACK
-static bool tm1637_write(tm1637_t *tm, uint8_t data) {
-  // oscillate DIO and clock while sending data
-  for (uint8_t i = 0, k = 1; i < sizeof(data); i++, k <<= 1) {
+static void write(tm1637_t *tm, uint8_t byte) {
+  for (uint8_t i = 0; i < 8; i++) {
     write_pin(tm->port, tm->clk, false);
     delay();
 
-    write_pin(tm->port, tm->dio, data & k);
+    write_pin(tm->port, tm->dio, byte & (1 << i));
     delay();
 
     write_pin(tm->port, tm->clk, true);
     delay();
   }
 
-  // wait for ACK
-  write_pin(tm->port, tm->clk, false);
-  write_pin(tm->port, tm->dio, true);
-  delay();
-
-  write_pin(tm->port, tm->clk, true);
-  delay();
-
-  // read ACK
-  bool ack = read_pin(tm->port, tm->dio);
-
-  if (!ack) {
-    write_pin(tm->port, tm->dio, false);
-  }
-
-  delay();
-
-  write_pin(tm->port, tm->clk, false);
-  delay();
-
-  return ack;
+  read_ack(tm);
 }
 
-// write data `data` starting at `pos` digit address
-static void tm1637_write_buffer(tm1637_t *tm, uint8_t *data, uint8_t size, uint8_t pos) {
-  // COMM1
-  tm1637_start(tm);
-  tm1637_write(tm, TM1637_COMM1);
-  tm1637_stop(tm);
+static void write_buffer(tm1637_t *tm, uint8_t buffer[], uint8_t length, uint8_t pos) {
+  start(tm);
+  write(tm, 0x40);
+  stop(tm);
 
-  // COMM2 + first digit address, data
-  tm1637_start(tm);
-  tm1637_write(tm, TM1637_COMM2 + (pos & 0x03));
+  start(tm);
+  write(tm, 0xc0 + (pos & 0x3));
 
-  for (uint8_t i = 0; i < size; i++) {
-    write_byte(tm, data[i]);
+  for (uint8_t i = 0; i < length; i++) {
+    write(tm, buffer[i]);
   }
 
-  tm1637_stop(tm);
+  stop(tm);
 
-  // COMM3 + brightness
-  tm1637_start(tm);
-  tm1637_write(tm, TM1637_COMM3 + (tm->brightness & 0xF));
-  tm1637_stop(tm);
+  start(tm);
+  write(tm, 0x80 + (tm->brightness & 0xf));
+  stop(tm);
 }
 
 void tm1637_init(tm1637_t *tm, port_t *port, pin_t clk, pin_t dio) {
   tm->port = port;
   tm->clk = clk;
   tm->dio = dio;
-  tm->brightness = 0;
+  tm->brightness = 8;
 
-  // set pins to default values
   write_pin(port, clk, false);
   write_pin(port, dio, false);
-
-  // set max brightness
-  tm1637_brightness(tm, 7);
 }
 
-void tm1637_brightness(tm1637_t *tm, uint8_t brightness, bool on) {
-  tm->brightness = (brightness & 0x7) | (on ? 0x08 : 0x00);
-}
+void tm1637_display(tm1637_t *tm, int32_t value, bool separator) {
+  // extract digits from value
+  uint8_t digits[4];
 
-void tm1637_write_int(tm1637_t *tm, int32_t digit, uint8_t pos) {
-  char str[7];
-  uint8_t buffer[TM1637_DIGITS] = {0};
+  for (int8_t i = sizeof(digits) - 1; i >= 0; i--) {
+    digits[i] = digit_masks[value % 10];
+    value /= 10;
 
-  // convert int to str
-  snprintf(str, sizeof(str), "%d", digit);
-
-  // convert to buffer
-  for (uint8_t i = 0; i < sizeof(buffer); i++) {
-    if (str[i] == '-') {
-      buffer[i] = 0x40;
-    } else if (str[i] >= '0' && str[i] <= '9') {
-      buffer[i] = digit_map[str[i] - '0'];
+    // enable central separator?
+    if (separator && i + 1 == sizeof(digits) / 2) {
+      digits[i] |= 1 << 7;
     }
   }
 
-  // write buffer to module
-  tm1637_write_buffer(tm, buffer, sizeof(buffer), pos);
+  // write segments
+  write_buffer(tm, digits, sizeof(digits), 0);
 }
 
-void tm1637_write_float(tm1637_t *tm, float digit, uint8_t dp, uint8_t pos) {
-  char str[8];
-  uint8_t buffer[TM1637_DIGITS] = {0};
-
-  // format string for printf
-  char format_str[] = "%.\0f";
-  format_str[2] = '0' + dp;
-
-  // convert float to str
-  snprintf(str, sizeof(str), format_str, digit);
-
-  // convert to buffer
-  for (uint8_t i = 0, j = 0; i <= sizeof(buffer); i++) {
-    if (str[i] == '-') {
-      buffer[j++] = 0x40;
-    } else if (str[i] >= '0' && str[i] <= '9') {
-      buffer[j++] = digit_map[str[i] - '0'];
-    } else if (str[i] == '.') {
-      if (j > 0) {
-        buffer[j - 1] |= 0x80;
-      }
-    } else {
-      buffer[j] = 0;
-      break;
-    }
-  }
-
-  // write buffer to module
-  tm1637_write_buffer(tm, buffer, sizeof(buffer), pos);
+void tm1637_brightness(tm1637_t *tm, uint8_t brightness) {
+  tm->brightness = brightness;
 }
--- code/shared/tm1637.h
+++ code/shared/tm1637.h
@@ -7,19 +7,19 @@ typedef struct {
   port_t *port;
   pin_t clk;
   pin_t dio;
-  uint8_t brightness; // brightness byte
+  uint8_t brightness;
 } tm1637_t;
 
 // initialise structure
 void tm1637_init(tm1637_t *tm, port_t *port, pin_t clk, pin_t dio);
 
-// set brightness from 0 to 7
-void tm1637_brightness(tm1637_t *tm, uint8_t brightness, bool on);
+// set brightness from 0 to 8
+void tm1637_brightness(tm1637_t *tm, uint8_t brightness);
 
-// display integer on display, starting at `pos`
-void tm1637_write_int(tm1637_t *tm, int32_t digit, uint8_t pos);
+// display integer on display
+void tm1637_display(tm1637_t *tm, int32_t digit, bool separator);
 
-// display float on display, starting at `pos`
-void tm1637_write_float(tm1637_t *tm, float digit, uint8_t dp, uint8_t pos);
+// externally defined delay function in micro-seconds
+extern void delay_us(uint32_t us);
 
 #endif
