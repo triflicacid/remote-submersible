9395134b33db4e4b46aa9f4f612333dda8e199cb
Sam Kennedy <112802187+Angus432@users.noreply.github.com>
2024-09-07 23:33:13 +0100

    disable interrupts for mcp spi

--- code/tests/integrates-off-board/Core/Src/Lib/mcp23s17.c
+++ code/tests/integrates-off-board/Core/Src/Lib/mcp23s17.c
@@ -10,22 +10,18 @@ void mcp_init(mcp_t *mcp, SPI_HandleTypeDef *spi, const pin_t *cs, uint8_t hw_ad
   mcp->hw_addr = hw_addr;
   mcp->rst = rst;
 
-  // toggle reset & chip select
+  // toggle chip select
   set_pin(cs);
-  //set_pin(rst);
   HAL_Delay(1);
-
   reset_pin(cs);
-  //reset_pin(rst);
   HAL_Delay(1);
-
   set_pin(cs);
-  //set_pin(rst);
   HAL_Delay(5);
 }
 
 void mcp_write(mcp_t *mcp, uint8_t address, uint8_t data) {
   reset_pin(mcp->cs);
+  HAL_Delay(1);
 
   uint8_t payload[] = {
     0x40, // get_control_byte(true, mcp->hw_addr),
@@ -33,7 +29,9 @@ void mcp_write(mcp_t *mcp, uint8_t address, uint8_t data) {
     data
   };
 
+  __disable_irq();
   HAL_SPI_Transmit(mcp->spi, payload, 3, MCP_TIMEOUT);
+  __enable_irq();
   HAL_Delay(1);
 
   set_pin(mcp->cs);
@@ -42,17 +40,19 @@ void mcp_write(mcp_t *mcp, uint8_t address, uint8_t data) {
 
 uint8_t mcp_read(mcp_t *mcp, uint8_t address) {
   reset_pin(mcp->cs);
+  HAL_Delay(1);
 
   uint8_t payload[] = {
     0x41, // get_control_byte(false, mcp->hw_addr),
     address
   };
 
+  __disable_irq();
   HAL_SPI_Transmit(mcp->spi, payload, 2, MCP_TIMEOUT);
-
   // read data sent back
   uint8_t data;
   HAL_SPI_Receive(mcp->spi, &data, 1, MCP_TIMEOUT);
+  __enable_irq();
   HAL_Delay(1);
 
   set_pin(mcp->cs);
